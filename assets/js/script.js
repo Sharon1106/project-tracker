b81e1c5b34271868a34909e4cdb82f181b2d3265
sharon <martinezsharonr@gmail.com>
2021-02-19 14:18:00 -0800

    added some text to time and date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 
 // Displays todays day and time on screen
 var today = moment();
-$("#lead").text(today.format("dddd, MMMM Do YYYY, h:mm:ss a"));
+$("#lead").text(today.format("[Today is]dddd, MMMM Do YYYY, h:mm:ss a"));
 
